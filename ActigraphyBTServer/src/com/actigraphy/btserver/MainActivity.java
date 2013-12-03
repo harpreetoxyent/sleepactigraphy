43fc921e98a6276a520e35e876489c0c91570ef1
ruchioxyent <ruchi_yadav@oxyent.com>
2013-12-03 10:26:41 +0530

    graph updated for every epoch

--- ActigraphyBTServer/src/com/actigraphy/btserver/MainActivity.java
+++ ActigraphyBTServer/src/com/actigraphy/btserver/MainActivity.java
@@ -35,10 +35,10 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity 
 {
-	private final String IP_ADDRESS_APP_SERVER="127.0.0.1";
-	private final String PORT_APP_SERVER = "8080";
-	private final String PROJECT_NAME_SERVER = "/ActigraphyAppServer/";
-	private final static String STORETEXT = "storetext.txt";
+	private final String IP_ADDRESS_APP_SERVER="192.168.1.4";
+	private final String PORT_APP_SERVER = "8085";
+	//private final String PROJECT_NAME_SERVER = "/ActigraphyAppServer/";
+	private final String PROJECT_NAME_SERVER = "/RecieveDataFromSleepApp/";
 	final int LENGTHOFARRAY = 15;
 	TextView info;
 	BluetoothAdapter mBluetoothAdapter;
@@ -105,7 +105,8 @@ public class MainActivity extends Activity
 			BluetoothSocket socket = null;
 			// Keep listening until exception occurs or a socket is returned
 			System.out.println("\nlistening: ");
-			while (true) {
+			while (true) 
+			{
 				try {
 					socket = mmServerSocket.accept();
 				} catch (IOException e) {
@@ -123,7 +124,7 @@ public class MainActivity extends Activity
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
-					break;
+					//break;
 				}
 			}
 		}
@@ -135,7 +136,7 @@ public class MainActivity extends Activity
 		System.out.println("\ninside manage connection..");
 		ConnectedThread ct = new ConnectedThread(socket);
 		ct.start();
-		ct.cancel();
+		//ct.cancel();
 	}
 
 	private class ConnectedThread extends Thread {
@@ -202,7 +203,7 @@ public class MainActivity extends Activity
 
 	class SendFile extends AsyncTask<String, Void, Void> {
 		protected Void doInBackground(String... urls) {
-			String existingFileName = STORETEXT;
+			
 			String URL = "http://"+IP_ADDRESS_APP_SERVER+":"+PORT_APP_SERVER+PROJECT_NAME_SERVER+"/SaveDataToMongoDB";
 
 			HttpClient httpclient = new DefaultHttpClient();
