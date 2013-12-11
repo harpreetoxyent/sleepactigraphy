7cbe613d928b9d2a9a2bb16c2e0a243a63716f06
ruchioxyent <ruchi_yadav@oxyent.com>
2013-12-11 18:23:39 +0530

    check

--- ActigraphyBTServer/gen/com/example/bluetoothserver/R.java
+++ ActigraphyBTServer/gen/com/example/bluetoothserver/R.java
@@ -24,9 +24,15 @@ public final class R {
         public static final int actigraphy=0x7f020000;
         public static final int actigraphy_serverdata=0x7f020001;
         public static final int ic_launcher=0x7f020002;
+        public static final int icon_about=0x7f020003;
+        public static final int icon_exit=0x7f020004;
+        public static final int icon_settings=0x7f020005;
     }
     public static final class id {
         public static final int action_settings=0x7f080001;
+        public static final int menu_About=0x7f080002;
+        public static final int menu_Exit=0x7f080004;
+        public static final int menu_Settings=0x7f080003;
         public static final int showInfo=0x7f080000;
     }
     public static final class layout {
@@ -34,6 +40,7 @@ public final class R {
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int menu=0x7f070001;
     }
     public static final class string {
         public static final int ActigraphyBTServer=0x7f050003;
