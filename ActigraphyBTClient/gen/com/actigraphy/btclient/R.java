92d1b2a0de1962209745206f817b7072ec4123b7
Harpreet Singh <harpreet_singh@oxyent.com>
2014-01-03 19:15:04 +0530

    Improvement in UI

--- ActigraphyBTClient/gen/com/actigraphy/btclient/R.java
+++ ActigraphyBTClient/gen/com/actigraphy/btclient/R.java
@@ -10,6 +10,11 @@ package com.actigraphy.btclient;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f060002;
+        public static final int gray=0x7f060000;
+        public static final int white=0x7f060001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -25,26 +30,39 @@ public final class R {
         public static final int actigraphy_sleepdata=0x7f020001;
         public static final int horizontal=0x7f020002;
         public static final int ic_launcher=0x7f020003;
-        public static final int vertical=0x7f020004;
+        public static final int rect_shape=0x7f020004;
+        public static final int vertical=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int layout=0x7f080000;
-        public static final int start=0x7f080004;
-        public static final int stop=0x7f080005;
-        public static final int x_axis=0x7f080001;
-        public static final int y_axis=0x7f080002;
-        public static final int z_axis=0x7f080003;
+        public static final int action_settings=0x7f09000b;
+        public static final int layout=0x7f090000;
+        public static final int start=0x7f090009;
+        public static final int stop=0x7f09000a;
+        public static final int tableRow1=0x7f090001;
+        public static final int tableRow2=0x7f090002;
+        public static final int tableRow3=0x7f090006;
+        public static final int tableRow4=0x7f090008;
+        public static final int timer=0x7f090007;
+        public static final int x_axis=0x7f090003;
+        public static final int y_axis=0x7f090004;
+        public static final int z_axis=0x7f090005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int gear_welcome_text=0x7f050005;
+        public static final int start=0x7f050003;
+        public static final int stop=0x7f050004;
+        public static final int timer=0x7f050002;
+        public static final int xaxis=0x7f050006;
+        public static final int yaxis=0x7f050007;
+        public static final int zaxis=0x7f050008;
     }
     public static final class style {
         /** 
@@ -68,10 +86,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
